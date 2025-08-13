5f807cc01c821be7e6cd2f6cc00b72c73480be91
AntDev210 <antonioieriti8@gmail.com>
2025-08-13 08:43:39 +0200

    Add files via upload

---
+++ js/announce.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const announcementList = document.getElementById('announcement-list');
+
+    fetch('db.json')
+        .then(response => response.json())
+        .then(data => {
+            if (data && data.announcements) {
+                const reversedAnnouncements = data.announcements.reverse();
+
+                reversedAnnouncements.forEach(announcement => {
+                    const postContainer = document.createElement('div');
+                    postContainer.className = 'post-container';
+                    postContainer.innerHTML = `
+                        <div class="post-header">
+                            <div class="user-info">
+                                <span class="username">${announcement.author}</span>
+                            </div>
+                            <span class="post-details">${announcement.date}</span>
+                        </div>
+                        <div class="post-body">
+                            <h3 class="post-title">${announcement.title}</h3>
+                            <p>${announcement.content}</p>
+                        </div>
+                    `;
+                    announcementList.appendChild(postContainer);
+                });
+            }
+        })
+        .catch(error => console.error('Errore durante il caricamento degli annunci:', error));
+});
